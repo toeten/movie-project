610926b2442327f7bfedcb4f99ddee2b3f77c8db
toeten <n.santiago0228@gmail.com>
2024-06-09 20:44:52 -0400

    WORK

--- movie-charts/vite.config.js
+++ movie-charts/vite.config.js
@@ -1,8 +1,9 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vite'
 
 export default defineConfig({
-  base: '/movie-project/',
-  build: {
-    outDir: 'dist'
-  }
+  // GitHub Pages expects an index.html in the root directory
+  // so just run npm build before pushing to GitHub and this will rebuild our assets to the root
+  build: { outDir: '..' },
+  // needed for github pages just put the repo name here
+  base: '/movie-project/', 
 });
