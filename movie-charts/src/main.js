f53c4a3e039ab6a1bd3bf8d8f4ca80bbff3b6129
toeten <n.santiago0228@gmail.com>
2024-06-09 20:53:52 -0400

    add

--- movie-charts/src/main.js
+++ movie-charts/src/main.js
@@ -1,6 +1,6 @@
 import { displayCards } from "./js/dom-manip";
 import { updateBarChart, updateDonutChart } from './js/dom-manip.js';
-import sampleMovies from "./movie-data.json"
+import sampleMovies from "../movie-data.json"
 
 const main = () => {
   displayCards();
